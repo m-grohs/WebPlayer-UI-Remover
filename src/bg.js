2d9c463ecf1349ffe470849cd39eba4337395908
mgrohs <marc.grohs@gmail.com>
2024-11-06 15:19:04 +0100

    update to manifest v3 for chrome fix netflix class changes

--- src/bg.js
+++ src/bg.js
@@ -1,3 +1,43 @@
 chrome.action.onClicked.addListener((tab) => {
-	chrome.tabs.sendMessage(tab.id, { cmd: 'delete' });
+	chrome.scripting.executeScript({
+		target: { tabId: tab.id },
+		function: hideUI
+	});
 });
+
+function hideUI() {
+	const SUPPORTED_SITES = ['amazon', 'disneyplus', 'netflix'];
+
+	if (SUPPORTED_SITES.some((site) => origin.includes(site))) {
+		// AMAZON
+		if (origin.includes(SUPPORTED_SITES[0])) {
+			const container = document.querySelector('.webPlayerUIContainer');
+
+			if (container) container.remove();
+		}
+
+		// DISNEY+
+		if (origin.includes(SUPPORTED_SITES[1])) {
+			const container = document.querySelector('.btm-media-overlays-container');
+
+			if (container) container.remove();
+		}
+
+		// NETFLIX
+		if (origin.includes(SUPPORTED_SITES[2])) {
+			const style = document.createElement('style');
+			style.textContent = `
+        		.ltr-1m81c36,
+				.ltr-1npqywr,
+				.ltr-1btbqeg,
+				.ltr-18ezbm2,
+				.ltr-1st24vv,
+				.advisory-container {
+            	visibility: hidden !important;}`;
+
+			document.head.appendChild(style);
+		}
+	} else {
+		console.error('This Site is not supported by the WebPlayer UI Remover Extension!');
+	}
+}
